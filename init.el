1b684d5bfd0e6e0841aa9df7f797def8fb51da10
EndlessDex <ampdexter@gmail.com>
2016-12-01 22:53:37 -0600

    Still testing

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 (require 'package)
+
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
