3b115266520f0332c76c891f85e5e6d36f6d6996
EndlessDex <ampdexter@gmail.com>
2018-05-20 14:02:05 -0500

    New life

--- backRand.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-if ping -q -c 1 -W 1 8.8.8.8 > /dev/null; then
-    wget -q https://source.unsplash.com/random/1920x1080 -O ~/backPic1.jpg;
-    wget -q https://source.unsplash.com/random/1680x1050 -O ~/backPic2.jpg;
-fi
